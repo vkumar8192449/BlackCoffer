8f259c36e932c994e05d1706dc2b115e46f3c931
vinay kumar <vkumar8192449@gmail.com>
2024-05-17 02:17:47 +0530

    frontend - countries list with flag

--- blackcoffer-frontend/src/components/Country.jsx
+++ blackcoffer-frontend/src/components/Country.jsx
@@ -1,5 +1,111 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const countryCodes = {
+  Algeria: "DZ",
+  Angola: "AO",
+  Argentina: "AR",
+  Australia: "AU",
+  Austria: "AT",
+  Azerbaijan: "AZ",
+  Belize: "BZ",
+  Brazil: "BR",
+  "Burkina Faso": "BF",
+  Canada: "CA",
+  China: "CN",
+  Colombia: "CO",
+  Cyprus: "CY",
+  Denmark: "DK",
+  Egypt: "EG",
+  Estonia: "EE",
+  Ethiopia: "ET",
+  Gabon: "GA",
+  Germany: "DE",
+  Ghana: "GH",
+  Greece: "GR",
+  Hungary: "HU",
+  India: "IN",
+  Indonesia: "ID",
+  Iran: "IR",
+  Iraq: "IQ",
+  Japan: "JP",
+  Jordan: "JO",
+  Kazakhstan: "KZ",
+  Kuwait: "KW",
+  Lebanon: "LB",
+  Liberia: "LR",
+  Libya: "LY",
+  Malaysia: "MY",
+  Mali: "ML",
+  Mexico: "MX",
+  Morocco: "MA",
+  Niger: "NE",
+  Nigeria: "NG",
+  Norway: "NO",
+  Oman: "OM",
+  Pakistan: "PK",
+  Poland: "PL",
+  Qatar: "QA",
+  Russia: "RU",
+  "Saudi Arabia": "SA",
+  "South Africa": "ZA",
+  "South Sudan": "SS",
+  Spain: "ES",
+  Syria: "SY",
+  Turkey: "TR",
+  Ukraine: "UA",
+  "United Arab Emirates": "AE",
+  "United Kingdom": "GB",
+  "United States of America": "US",
+  Venezuela: "VE",
+};
 
 export const Country = () => {
-  return <div>Country</div>;
+  const [country, setcountry] = useState();
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_APP_BACKEND_URL}/api/v1/country`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      );
+      console.log(response?.data?.data);
+      setcountry(response?.data?.data?.countries);
+    } catch (error) {
+      console.error(error.response?.data?.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <div className="mt-14 border border-gray-400 rounded-lg w-[22vw] max-h-[70vh] overflow-y-scroll no-scrollbar bg-white">
+      <p className="text-3xl mt-4 ml-6 mb-4">Countries</p>
+      {country?.map((item, key) => (
+        <div
+          key={key}
+          className="mb-3 cursor-pointer flex items-center rounded-lg w-[20vw]"
+        >
+          <img
+            className="w-9 h-9 rounded-full ml-8"
+            src={`https://flagcdn.com/48x36/${countryCodes[
+              item._id
+            ].toLowerCase()}.png`}
+          />
+          <div className="ml-4">
+            <div className="text-md font-semibold">{item._id}</div>
+            <div className="text-xs font-medium">
+              Number of records {item.count}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
